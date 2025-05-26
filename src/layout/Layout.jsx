26106c06beb019510b5649025a2fdd9661c38d8c
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-05-26 19:52:53 +0600

    Implement responsive navbar and footer layout

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Layout = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar></Navbar>
+      <Outlet> {/* Pages */}</Outlet>
+      {/* Footer */}
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Layout;
